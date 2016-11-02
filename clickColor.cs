a9f0c6ec611e96f0cbc5b705098aeeaee0ff1f35
Bernat Ferragut <elbernat@gmail.com>
2016-11-02 11:25:41 -0400

    cs clickColor script

---
+++ clickColor.cs
@@ -0,0 +1,27 @@
+﻿// Script to change the color of an object
+// ***************************************
+
+using UnityEngine;
+using System.Collections;
+
+public class clickColor : MonoBehaviour {
+
+	// variables
+	Color originalColor;
+	public Color color = Color.red;
+
+	// functions
+	void Start () {
+		originalColor = renderer.material.color;
+	}
+
+	void OnMouseDown () {
+
+		if (renderer.material.color == originalColor){
+			renderer.material.color == color;
+		} else {
+			renderer.material.color = originalColor;
+		}
+	}
+
+// *********************************************
