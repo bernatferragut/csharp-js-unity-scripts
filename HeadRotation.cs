81dce88151894a18dfd5b2cbb004dcef6474da40
Bernat Ferragut <elbernat@gmail.com>
2016-11-03 08:10:32 -0400

    VR Head Rotation simple script

---
+++ HeadRotation.cs
@@ -0,0 +1,22 @@
+﻿//Script that captures our head rotation from the Gyroscope Info.
+// **************************************************************
+
+using UnityEngine;
+using System.Collections;
+
+public class HeadRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true; 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quatternion att = Input.gyro.attitude;
+		att = Quaternion.Euler(90f, 0f, 0f) * new Quaternion (att.x, att.y, -att.z, -att.w);
+		transform.rotation = att;
+	
+	}
+}
