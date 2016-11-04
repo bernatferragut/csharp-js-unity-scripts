ad1d68389e8ff38595f7380a75b8e12a45c70949
Bernat Ferragut <elbernat@gmail.com>
2016-11-04 10:55:39 -0400

    Collision Script - First Part

---
+++ TouchSensitive.cs
@@ -0,0 +1,17 @@
+﻿// Collision script
+// ***************************************
+
+using UnityEngine;
+using System.Collections;
+
+// add this to the player
+public class TouchSensitive : MonoBehavoiur {
+
+    void OnControllerCollideHit( ControllerCollideHit hit ){
+        Damage dmg = hit.gameObject.GetComponent<Damage>();
+            if( dmg != null){
+                dmg.DoDamage(this.gameObject);
+            }
+    }
+}
+// *********************************************
