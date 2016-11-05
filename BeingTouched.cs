4a7bca29fecc5f6601b0f33d897850d8011ed2c0
Bernat Ferragut <elbernat@gmail.com>
2016-11-05 11:09:28 -0400

    BeingTouched or Damaged script

---
+++ BeingTouched.cs
@@ -0,0 +1,16 @@
+﻿// Being Touched script
+// ***************************************
+
+using UnityEngine;
+using System.Collections;
+
+// Put this on something that touches the player
+public class BeingTouched : MonoBehavoiur {
+    public void DoTouch( GameObject go ){
+        Fall f = go.GetComponent<Fall>();
+            if(f != null){
+                go.transform.position = new Vector3(0, f.lowestAllowed, 0);
+            }
+    }
+}
+// *********************************************
