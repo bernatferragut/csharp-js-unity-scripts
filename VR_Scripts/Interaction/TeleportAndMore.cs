7364dbf16128c7a7f2844fbf3d348077f0fdf7e7
Bernat Ferragut <elbernat@gmail.com>
2016-11-24 16:43:15 -0400

    New Added Scripts

--- VR_Scripts/Interaction/TeleportAndMore.cs
+++ VR_Scripts/Interaction/TeleportAndMore.cs
@@ -2,67 +2,67 @@
  
 using UnityEngine;
 using System.Collections;
-[RequireComponent(typeof(Collider))]
 
+[RequireComponent(typeof(Collider))]
 public class Test : MonoBehaviour, IGvrGazeResponder
 {
     private Vector3 startingPosition;
-//Start Game  
+
     void Start()
     {
-    startingPosition = transform.localPosition;
-    SetGazedAt(false);
+        startingPosition = transform.localPosition;
+        SetGazedAt(false);
     }
     
     void LateUpdate()
     {
-    GvrViewer.Instance.UpdateState();
-    if (GvrViewer.Instance.BackButtonPressed)
-    {
-    Application.Quit();
-    }
+        GvrViewer.Instance.UpdateState();
+        if (GvrViewer.Instance.BackButtonPressed)
+            {
+                Application.Quit();
+            }
     }
     
     public void SetGazedAt(bool gazedAt)
     {
-    GetComponent().material.color = gazedAt ? Color.green : Color.red;
+        GetComponent().material.color = gazedAt ? Color.green : Color.red;
     }
     public void Reset()
     {
-    transform.localPosition = startingPosition;
+        transform.localPosition = startingPosition;
     }
     public void ToggleVRMode()
     {
-    GvrViewer.Instance.VRModeEnabled = !GvrViewer.Instance.VRModeEnabled;
+        GvrViewer.Instance.VRModeEnabled = !GvrViewer.Instance.VRModeEnabled;
     }
     public void ToggleDistortionCorrection()
     {
-    switch (GvrViewer.Instance.DistortionCorrection)
-    {
-        case GvrViewer.DistortionCorrectionMethod.Unity:
-        GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.Native;
-        break;
-        case GvrViewer.DistortionCorrectionMethod.Native:
-        GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.None;
-        break;
-        case GvrViewer.DistortionCorrectionMethod.None:
-        default:
-        GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.Unity;
-        break;
-    }
+        switch (GvrViewer.Instance.DistortionCorrection)
+        {
+            case GvrViewer.DistortionCorrectionMethod.Unity:
+                GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.Native;
+            break;
+            case GvrViewer.DistortionCorrectionMethod.Native:
+                GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.None;
+            break;
+            case GvrViewer.DistortionCorrectionMethod.None:
+            default:
+            GvrViewer.Instance.DistortionCorrection = GvrViewer.DistortionCorrectionMethod.Unity;
+            break;
+        }
     }
     
     public void ToggleDirectRender()
     {
-    GvrViewer.Controller.directRender = !GvrViewer.Controller.directRender;
+        GvrViewer.Controller.directRender = !GvrViewer.Controller.directRender;
     }
     
     public void TeleportRandomly()
     {
-    Vector3 direction = Random.onUnitSphere;
-    direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
-    float distance = 2 * Random.value + 1.5f;
-    transform.localPosition = direction * distance;
+        Vector3 direction = Random.onUnitSphere;
+        direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
+        float distance = 2 * Random.value + 1.5f;
+        transform.localPosition = direction * distance;
     }
     
     #region IGvrGazeResponder implementation
@@ -71,20 +71,20 @@ public class Test : MonoBehaviour, IGvrGazeResponder
     /// as long as it is set to an appropriate layer (see GvrGaze).
     public void OnGazeEnter()
     {
-    SetGazedAt(true);
+        SetGazedAt(true);
     }
     
     /// Called when the user stops looking on the GameObject, after OnGazeEnter
     /// was already called.
     public void OnGazeExit()
     {
-    SetGazedAt(false);
+        SetGazedAt(false);
     }
     
     /// Called when the viewer's trigger is used, between OnGazeEnter and OnGazeExit.
     public void OnGazeTrigger()
     {
-    TeleportRandomly();
+        TeleportRandomly();
     }
     #endregion
 }
