4cdd3719822d08ddd69a37ea60b0aa368a3ce9b7
Bernat Ferragut <elbernat@gmail.com>
2016-11-28 07:18:55 -0400

    Singleton Pattern cs script

---
+++ Basic_Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton : MonoBehaviour {
+
+	private int _currentScore;
+	// Here we declare the one and only instance possible called the singleton
+	public static MainGameManager instance;
+
+	void Awake()
+	{	
+		instance.this; // Here we create the one and only instance possible called the singleton
+	}
+
+
+	public adjustScore( int num)
+	{
+		_currentScore = _currentScore + num;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (10, 100, 100, 100), "Score = " + _currentScore);
+	}
+
+}
