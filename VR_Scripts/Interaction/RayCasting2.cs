0f9f88f0ad762f8d47b593372d685b73ae5154bb
Bernat Ferragut <elbernat@gmail.com>
2016-11-30 09:53:52 -0400

    New script: 01. Scripts as Behaviours Components

---
+++ VR_Scripts/Interaction/RayCasting2.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCasting2 : MonoBehaviour {
+
+	// Initialize varaibles
+
+	public string objectCollided;
+	public TextMesh textMessage;
+
+	void Start () 
+	{
+		textMessage = GameObject.Find("TextPanel").GetComponent<TextMesh> ();
+		textMessage.text = "nothing";
+	}
+
+	// We RayCast
+
+	public void FixedUpdate() 
+	{
+		RaycastHit hit; // the one who stores hit info - very important !!!
+		Vector3 origin = transform.position;
+		Vector3 direction = transform.TransformDirection(Vector3.forward);
+		Ray myRay = new Ray (origin, direction);
+
+		//If we collide we announce the object collided to the Console and to a 3DText
+
+		if (Physics.Raycast (myRay, out hit, 100f)) 
+		{
+			objectCollided = hit.collider.gameObject.name;
+
+			print (objectCollided); // == Debug.Log in Console
+
+			textMessage.text = objectCollided; // printing to the 3D Text.TextMesh.text
+		} 
+		else 
+		{
+			textMessage.text = "nothing"; // printing to the 3D Text.TextMesh.text
+		}
+	}
+		
+}
+
---
+++ VR_Scripts/UnitySnippetsDB.cs
@@ -0,0 +1,29 @@
+// Beginner Scripting Basics Snippets Guide                 //
+// Compilation from Unity 'Scripts as Behaviours Components'//
+//**********************************************************//
+
+//01. Scripts as Behaviours Components
+
+using UnityEngine;
+using System.Collections;
+
+public class ExampleBehaviourScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
+
+//02. Variables and Functions
